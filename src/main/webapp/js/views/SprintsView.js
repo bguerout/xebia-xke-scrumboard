7b9d46002388480fa5e7551a1722daac8ee2038b
Benoit Guerout <bguerout@gmail.com>
2012-09-05 19:51:19 +0200

    Revert "Backbone"
    This reverts commit f257a118c668ff6181a355a169e2c440af8cad26.

--- src/main/webapp/js/views/SprintsView.js
+++ src/main/webapp/js/views/SprintsView.js
@@ -8,32 +8,12 @@ define(['text!/tmpl/tmpl.html'], function (tmpl) {
             'click .deletesprint': 'delete'
         },
     	initialize: function() {
-    		this.collection.on('reset sync remove', this.render, this);
     	},
     	render: function() {
-            this.$el.empty();
-
-            var view = this;
-    		this.collection.each(function(sprint) {
-    			view.$el.append(view.template(sprint.toJSON()));
-    		});
-
-            this.$el.append(view.createTemplate);
     	},
         create: function() {
-            var name = this.$el.find('input[type=text]').val();
-            this.collection.create({name:name});
         },
         delete: function(event) {
-            var story = $(event.target).closest('.DashboardStory');
-            
-            var view = this;
-            $(story.parent().children()).each(function(index, item) {
-                if($(item)[0] == story[0]) {
-                    var model = view.collection.at(index);
-                    model.destroy({wait: true});
-                }
-            });
         }
     })
 });
