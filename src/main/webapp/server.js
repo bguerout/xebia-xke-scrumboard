9110766e5c875a3b10e486042cf0d9d0a8233d10
Benoit Guerout <bguerout@gmail.com>
2012-09-05 19:51:41 +0200

    Revert "Nodejs"
    This reverts commit 2519581179efe2efc6fd4b967ffa2881bf24b554.

--- src/main/webapp/server.js
+++ src/main/webapp/server.js
@@ -12,33 +12,18 @@ app.use(express.static(__dirname));
 app.resource('resource/sprint', {
   // GET /sprints
   index: function(req, res) {
-    db.sprints.find({}, function(err, sprints) {
-      res.send(sprints);
-    });
   },
   // POST /sprints
   create: function(req, res) {
-    db.sprints.save(req.body, function(err, sprint) {
-      res.send(sprint, 201);
-    });
   },
   // GET /sprints/id
   show: function(req, res) {
-    db.sprints.findOne({_id: db.ObjectId(req.params.sprint)}, function(err, sprint) {
-      res.send(sprint);
-    });
   },
   // PUT /sprints/id
   update: function(req, res) {
-    db.sprints.update({_id: db.ObjectId(req.params.sprint)}, {$set: req.body}, function(err, sprint) {
-      res.send(sprint);
-    });
   },
   // DELETE /sprints/id
   destroy: function(req, res) {
-    db.sprints.remove({_id: db.ObjectId(req.params.sprint)}, function(err, sprint) {
-      res.send();
-    });
   }
 });
 
