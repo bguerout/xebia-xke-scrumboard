def186e0d36f9292af41b77556b6f1affcb30c88
Benoit Guerout <bguerout@gmail.com>
2012-09-05 19:42:45 +0200

    Jersey Solution. #Cheat commit#

--- src/main/java/com/xebia/scrumboard/resource/SprintResource.java
+++ src/main/java/com/xebia/scrumboard/resource/SprintResource.java
@@ -1,26 +1,28 @@
 package com.xebia.scrumboard.resource;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
-
+import com.sun.jersey.api.NotFoundException;
+import com.xebia.scrumboard.data.Sprints;
 import com.xebia.scrumboard.representation.Sprint;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("sprint")
 @Produces(APPLICATION_JSON)
 public class SprintResource {
-
     @GET
     @Path("{id}")
     public Response get(@PathParam("id") String id) {
-        return Response.ok().build();
+        Sprint sprint = Sprints.get(id);
+
+        if (sprint == null)
+            throw new NotFoundException();
+
+        return Response.ok(sprint).build();
     }
 
     @PUT
@@ -31,16 +33,22 @@ public class SprintResource {
 
     @POST
     public Response post(Sprint sprint) {
-        return Response.ok().build();
+        Sprints.put(sprint);
+        return Response.ok(sprint).build();
     }
 
     @DELETE
     @Path("{id}")
     public void delete(@PathParam("id") String id) {
+        Sprints.delete(id);
     }
 
     @GET
     public Response get() {
-        return Response.ok().build();
+        List<Sprint> sprints = Sprints.get();
+
+        GenericEntity<List<Sprint>> entity = new GenericEntity<List<Sprint>>(sprints) {
+        };
+        return Response.ok(entity).build();
     }
 }
